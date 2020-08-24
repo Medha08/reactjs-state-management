c0df729fb60750a72f3c75945f094b99c7d545a8
Medha08 <medha8pandey@gmail.com>
2020-08-24 15:47:43 +0530

    Structure App

---
+++ React/ContextAPI/languageform/src/components/App.js
@@ -0,0 +1,28 @@
+import React from "react";
+import UserCreate from "./UserCreate";
+
+class App extends React.Component {
+  state = { language: "english" };
+  onLanguageChange = (language) => {
+    this.setState({ language });
+  };
+  render() {
+    return (
+      <div className="ui container">
+        <div>
+          Select a language:
+          <i
+            className="flag us"
+            onClick={() => this.onLanguageChange("english")}
+          ></i>
+          <i
+            className="flag in"
+            onClick={() => this.onLanguageChange("hindi")}
+          ></i>
+        </div>
+        <UserCreate></UserCreate>
+      </div>
+    );
+  }
+}
+export default App;
