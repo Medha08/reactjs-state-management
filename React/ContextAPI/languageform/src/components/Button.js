54619eb7c14db173797d2098fe7de0e0c8412ad9
Medha08 <medha8pandey@gmail.com>
2020-08-24 17:05:05 +0530

    using Consumer Component

--- React/ContextAPI/languageform/src/components/Button.js
+++ React/ContextAPI/languageform/src/components/Button.js
@@ -2,10 +2,20 @@ import React from "react";
 import LanguageContext from "../contexts/LanguageContext";
 
 class Button extends React.Component {
-  static contextType = LanguageContext;
+  //   static contextType = LanguageContext;
+  renderSubmit = (value) => {
+    return value === "english" ? "Submit" : "प्रस्तुत";
+  };
   render() {
-    const btnText = this.context === "english" ? "Submit" : "प्रस्तुत";
-    return <button className="ui button primary">{btnText}</button>;
+    // const btnText = this.context === "english" ? "Submit" : "प्रस्तुत";
+
+    return (
+      <button className="ui button primary">
+        <LanguageContext.Consumer>
+          {(value) => this.renderSubmit(value)}
+        </LanguageContext.Consumer>
+      </button>
+    );
   }
 }
 
